486e3832fead731f17108c596ffdaa066b84bdaf
Saksham Yadav <ssysaksham@gmail.com>
2022-02-06 18:39:22 +0530

    FilesAddition

---
+++ LogisticRegression.py
@@ -0,0 +1,30 @@
+class LogisticRegression:
+    
+    def sigmoid(self, z): return 1 / (1 + e**(-z))
+    
+    def cost_function(self, X, y, weights):                 
+        z = dot(X, weights)        
+        predict_1 = y * log(self.sigmoid(z))
+        predict_0 = (1 - y) * log(1 - self.sigmoid(z))        
+        return -sum(predict_1 + predict_0) / len(X)
+    
+    def fit(self, X, y, epochs=25, lr=0.05):        
+        loss = []
+        weights = rand(X.shape[1])
+        N = len(X)
+                 
+        for _ in range(epochs):        
+            # Gradient Descent
+            y_hat = self.sigmoid(dot(X, weights))
+            weights -= lr * dot(X.T,  y_hat - y) / N            
+            # Saving Progress
+            loss.append(self.cost_function(X, y, weights)) 
+            
+        self.weights = weights
+        self.loss = loss
+    
+    def predict(self, X):        
+        # Predicting with sigmoid function
+        z = dot(X, self.weights)
+        # Returning binary result
+        return [1 if i > 0.5 else 0 for i in self.sigmoid(z)]
